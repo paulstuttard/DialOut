82c80769d940d1ca19210ef0911721c10e0273cf
Paul Stuttard <paul@macbook-pro.local>
2018-02-11 17:18:17 +0000

    add getNumbers initial prototype

--- lambda/custom/asterisk.js
+++ lambda/custom/asterisk.js
@@ -2,15 +2,33 @@ var request = require("request");
 
 function Asterisk() {
   var proxyURL = "";
-  var userName = "";
-  var password = "";
+  var proxyUsername = "";
+  var proxyPassword = "";
   var contactsQuery = "?Name=";
   var dialQuery = "?Number=";
 
   function connect(url, pass, user) {
-    userName = user;
-    password = pass;
+    proxyUsername = user;
+    proxyPassword = pass;
     proxyURL = url;
   }
-  
+
+  function getNumbers(phrase, callback) {
+    var url = proxyURL + contactsQuery + phrase;
+    var options = {
+      uri : url,
+      method : 'GET',
+      auth : {
+        username : proxyUsername,
+        password : proxyPassword
+      }
+    }
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var results = JSON.parse(body);
+        callback(results);
+      }
+
+    }
+  }
 }
