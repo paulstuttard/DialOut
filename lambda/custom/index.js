e9c194a5367bd656c6506e73e9f52005d0126c7e
Paul Stuttard <paul@macbook-pro.local>
2018-02-24 22:23:27 +0000

    Add possessive names, break out queryRing

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -10,14 +10,58 @@ exports.handler = function(event, context) {
     alexa.execute();
 };
 
-function namePronounce(name) {
+//
+// Alexa can't manage these words, so provide some assistance
+//
+function wordPronounce(name, possessive) {
   // borS"in
   // stVttArd
-  name = name.replace("Stuttard", "<phoneme alphabet=\"x-sampa\" ph=\"stVttArd\">Stuttard</phoneme>");
-  name = name.replace("Bourgein", "<phoneme alphabet=\"x-sampa\" ph='borS\"in'>Bourgein</phoneme>");
+  if (possessive) {
+    name = name.replace("Bourgein", "<phoneme alphabet=\"x-sampa\" ph='borS\"inz'>Bourgein's</phoneme>");
+    name = name.replace("Stuttard", "<phoneme alphabet=\"x-sampa\" ph=\"stVtArdz\">Stuttard's</phoneme>");
+  } else {
+    name = name.replace("Stuttard", "<phoneme alphabet=\"x-sampa\" ph=\"stVtArd\">Stuttard</phoneme>");
+    name = name.replace("Bourgein", "<phoneme alphabet=\"x-sampa\" ph='borS\"in'>Bourgein</phoneme>");
+  }
   return name;
 }
 
+//
+// Query the first/next name that has a phone number
+//
+function queryRing(alexa, firstTime) {
+
+  if (firstTime) {
+    this.attributes.callIndex = 0;
+  } else {
+    this.attributes.callIndex++;
+  }
+
+  var output = "Do you want me to ring ";
+  var index = alex.attributes.callIndex;
+  while (index < alex.attributes.results.length)
+  {
+    if (alex.attributes.results[index].Number) {
+      alex.attributes.callIndex = index;
+      var name = alex.attributes.results[index].Fullname;
+      output += wordPronounce(name, false) + '?';
+      console.log("Try contact: ", alex.attributes.results[index].Fullname);
+      alex.emit(':ask', output);
+    }
+    index++;
+  }
+  if (firstTime) {
+    output = "Sorry, I could not find a contact matching " + wordPronounce(alexa.attributes.phrase, false);
+  } else {
+    output = "OK, that was the last match, bye."
+  }
+  this.emit(':tell', output);
+}
+
+//
+// Check authorization is valid and get the server details from the user's
+// account.
+//
 function checkAuth(alexa) {
   if (alexa.event.session.user.accessToken) {
     var options =  {
@@ -54,19 +98,24 @@ var handlers = {
       this.attributes.myState = 'RINGCONTACT';
       checkAuth(this);
     },
+    'ContactBirthday': function () {
+      this.attributes.myState = 'GETBIRTHDAY';
+      checkAuth(this);
+    },
     'FindContact' : function () {
 
-      // This intent can have a few different slots
-      // (nickname| name) [type]
       var phrase = "";
       if (this.event.request.intent.slots.name &&
           this.event.request.intent.slots.name.value) {
         phrase = phrase + " " + this.event.request.intent.slots.name.value;
+        console.log("Slot[name]: ", this.event.request.intent.slots.name.value)
       }
       if (this.event.request.intent.slots.type &&
           this.event.request.intent.slots.type.value) {
         phrase = phrase + " " + this.event.request.intent.slots.type.value;
+        console.log("Slot[name]: ", this.event.request.intent.slots.type.value)
       }
+      this.attributes.phrase = phrase;
       var Proxy = new Asterisk();
       Proxy.connect(this.attributes.url, this.attributes.user, this.attributes.pass);
       Proxy.getNumbers(phrase , (results) => {
@@ -84,19 +133,19 @@ var handlers = {
           // Got some results, ask them one at a time if they want to ring them
           if (this.attributes.myState == 'RINGCONTACT') {
             this.attributes.myState = 'CHECKCONTACT';
-            this.attributes.callIndex = 0;
-
-            var output = "Do you want me to ring ";
-            var name = this.attributes.results[this.attributes.callIndex].Fullname;
-            output += namePronounce(name) + '?';
-            console.log("Try contact: ", this.attributes.results[this.attributes.callIndex].Fullname);
-            this.emit(':ask', output);
+            queryRing(this, true);
 
           } else if (this.attributes.myState == 'GETADDRESS'){
             var name = this.attributes.results[0].Fullname;
-            var output = namePronounce(name);
-            output += "'s address is <prosody rate=\"slow\"><say-as interpret-as=\"address\">";
-            output += this.attributes.results[0].Address + "</say-as></prosody>";
+            var output = wordPronounce(name, true);
+            output += " address is, "
+            output += this.attributes.results[0].Address;
+            this.emit(':tell', output);
+          } else if (this.attributes.myState == 'GETBIRTHDAY'){
+            var name = this.attributes.results[0].Fullname;
+            var output = wordPronounce(name, true);
+            output += " birthday is <say-as interpret-as=\"date\">";
+            output += this.attributes.results[0].Birthday + "</say-as>";
             this.emit(':tell', output);
           }
         }
@@ -109,7 +158,7 @@ var handlers = {
       if (this.attributes.myState == 'CHECKCONTACT') {
         var output = "Dialing ";
         var name = this.attributes.results[this.attributes.callIndex].Fullname;
-        output += namePronounce(name);
+        output += wordPronounce(name,false);
         //
         var Proxy = new Asterisk();
         Proxy.connect(this.attributes.url, this.attributes.user, this.attributes.pass);
@@ -124,25 +173,14 @@ var handlers = {
     },
     'AMAZON.NoIntent' : function() {
       if (this.attributes.myState == 'CHECKCONTACT') {
-        this.attributes.callIndex++;
-        if (this.attributes.callIndex >= this.attributes.results.length){
-          this.emit(':tell', 'OK, bye.');
-        } else {
-          var output = "Do you want me to ring ";
-          var name = this.attributes.results[this.attributes.callIndex].Fullname;
-          output += namePronounce(name) + "?";
-          console.log("Try contact: ", this.attributes.results[this.attributes.callIndex].Fullname);
-          this.emit(':ask', output);
-        }
-      } else {
-        this.emit(':tell', 'OK, bye.');
+        dialQuery(this, false);
       }
     },
     'AMAZON.StopIntent' : function() {
       this.emit(':tell', 'OK, bye.');
     },
     'AMAZON.HelpIntent' : function() {
-      this.emit(':ask', "You can try: 'alexa, start dialing David' or 'alexa, start dialing David at home'");
+      this.emit(':ask', "You can try: 'ring David' or 'ring David at home' or 'ask address book for David's address' or 'ask address book for David's Birthday'");
     },
     'AMAZON.CancelIntent' : function() {
       this.emit(':tell', 'OK, bye.');
@@ -153,11 +191,3 @@ var handlers = {
       this.emit(':responseReady');
     }
 };
-
-/*
-var Proxy = new Asterisk();
-Proxy.connect("https://stutty.zapto.org/asterisk/dial.php", "paul", "foxtrot1");
-Proxy.getNumbers("David", function(results) {
-  console.log("Results:", results);
-});
-*/
